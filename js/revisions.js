d6b206f2db14a15e69c1d62224b7242e949f18f7
João Campos <joao.campos@codingskills.com>
2017-11-03 11:08:22 +0100

    fix JS exiting too soon when accepted revision is the last

--- js/revisions.js
+++ js/revisions.js
@@ -21,61 +21,29 @@
 				break;
 			}
 		}
-		if (acceptedIndex >= revisionData.length - 1) { return; }
 
 		// Mark the accepted revision visually
-		$('.revisions-tickmarks div:nth-child(' + (acceptedIndex + 1) + ')').css({
-			borderLeft: '3px solid #46b450'
-		});
-		var acceptedPosition = acceptedIndex / (revisionData.length - 1) * 100,
-			$pendingChangesTickmarks = $('<span class="fcr-current-revision-tickmark">');
-		$pendingChangesTickmarks.css({
-			position: 'absolute',
-			height: '100%',
-			'-webkit-box-sizing': 'border-box',
-			'-moz-box-sizing': 'border-box',
-			boxSizing: 'border-box',
-			display: 'block',
-			left: acceptedPosition + '%',
-			width: (100 - acceptedPosition) + '%',
-			border: 'none',
-			background: 'repeating-linear-gradient(-60deg, #ddd, #ddd 9px, #f7f7f7 10px, #f7f7f7 17px)',
-			pointerEvents: 'none',
-		});
-		$('.revisions-tickmarks').prepend($pendingChangesTickmarks);
-
-		// [TODO] remove (will be rendered obsolete by columns headers)
-		// Disable Restore revision button for last revisions
-		var sliding = false, sliderData = { value: null, values: null};
-		// Values retrieved from Slider are not always up-to-date so get them from the event itself
-		$('.wp-slider').on('slide change', function(event, ui) {
-			sliderData.value = ui.value;
-			sliderData.values = ui.values;
-		});
-		// Detect if button was changed
-		var disableLastRevisionRestore = function(event) {
-			if (sliding) { return; } // Triggered by a previous call to this function
-			sliding = true;
-			var $slider = $('.wp-slider'),
-				value = sliderData.value !== null ? sliderData.value : $slider.slider('value'),
-				values = sliderData.values !== null ? sliderData.values : $slider.slider('values'),
-				latestRevision = revisionData.length - 1;
-			if (values && values.length > 0) {
-				value = isRtl ? values[0] : values[1];
-			}
-			var position = isRtl ? latestRevision - value : value;
-
-			if (revisionData[latestRevision].autosave) {
-				// Autosave can be restored but lastest revision excluding autosave can't
-				latestRevision--;
-			}
-			if (position == latestRevision) {
-				$('.restore-revision.button').attr('disabled', true);
-			}
-			sliding = false;
-		};
-		disableLastRevisionRestore();
-		$('.diff-meta-to').on('DOMSubtreeModified', disableLastRevisionRestore);
+		if (acceptedIndex < revisionData.length - 1) {
+			$('.revisions-tickmarks div:nth-child(' + (acceptedIndex + 1) + ')').css({
+				borderLeft: '3px solid #46b450'
+			});
+			var acceptedPosition = acceptedIndex / (revisionData.length - 1) * 100,
+				$pendingChangesTickmarks = $('<span class="fcr-current-revision-tickmark">');
+			$pendingChangesTickmarks.css({
+				position: 'absolute',
+				height: '100%',
+				'-webkit-box-sizing': 'border-box',
+				'-moz-box-sizing': 'border-box',
+				boxSizing: 'border-box',
+				display: 'block',
+				left: acceptedPosition + '%',
+				width: (100 - acceptedPosition) + '%',
+				border: 'none',
+				background: 'repeating-linear-gradient(-60deg, #ddd, #ddd 9px, #f7f7f7 10px, #f7f7f7 17px)',
+				pointerEvents: 'none',
+			});
+			$('.revisions-tickmarks').prepend($pendingChangesTickmarks);
+		}
 
 		// Change columns' header cards
 		var $revisionsHeaders = $('<div>', {class: 'revisions-headers'});
@@ -102,9 +70,7 @@
 		var renderColumnHeaders = function(value, values) {
 			var latestRevision = revisionData.length - 1;
 			if (!values || values.length <= 0) {
-				values = [];
-				values[0] = value - 1;
-				values[1] = value;
+				values = [value - 1, value];
 			}
 
 			var $headerFrom = renderColumnHeader('from', revisionData[values[0]]),
