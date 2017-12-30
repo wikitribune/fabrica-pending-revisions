be07e05fe4916adcb24bb3be7a2fc8785913df84
João Campos <joao.campos@codingskills.com>
2017-12-30 17:38:27 +0000

    update version

--- fabrica-pending-revisions.php
+++ fabrica-pending-revisions.php
@@ -3,7 +3,7 @@
 Plugin Name: Fabrica Pending Revisions
 Plugin URI:
 Description:
-Version: 0.0.15
+Version: 0.0.16
 Author: Fabrica
 Author URI: https://fabri.ca/
 Text Domain: fabrica-pending-revisions
