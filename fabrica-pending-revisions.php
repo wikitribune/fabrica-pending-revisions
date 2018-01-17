ff9c1c368f62fed705160e9485c2dea031003481
Andrew Staffell <andrew@yeswework.com>
2018-01-17 13:56:55 +0100

    update version

--- fabrica-pending-revisions.php
+++ fabrica-pending-revisions.php
@@ -3,7 +3,7 @@
 Plugin Name: Fabrica Pending Revisions
 Plugin URI:
 Description:
-Version: 0.0.16
+Version: 0.0.17
 Author: Fabrica
 Author URI: https://fabri.ca/
 Text Domain: fabrica-pending-revisions
